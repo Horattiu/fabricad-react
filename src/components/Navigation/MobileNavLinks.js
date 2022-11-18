c3676276422e20e5892452b52e1c525cef494747
Oltean Horatiu <horatiu.oltn@gmail.com>
2022-11-18 14:01:03 +0200

    mobilemenufix

--- src/components/Navigation/MobileNavLinks.js
+++ src/components/Navigation/MobileNavLinks.js
@@ -5,19 +5,20 @@ import React from "react";
 
 const MobileNavLinks = ({ setIsCartOpen, closeMobileMenu, isMobile }) => {
   return (
-    <div className="links-mobile">
-      <Link to="/despre" onClick={() => isMobile && closeMobileMenu()}>
-        DESPRE
-      </Link>
-      <Link to="/produse" onClick={() => isMobile && closeMobileMenu()}>
-        PRODUSE
-      </Link>
-      <Link to="/proiecte" onClick={() => isMobile && closeMobileMenu()}>
-        PROIECTE
-      </Link>
-      <div onClick={() => setIsCartOpen(true)}>
-        COS DE CUMPARATURI
-        <FiShoppingCart className="cart-icon" />
+    <div className="mobile-modal">
+      <div className="links-mobile">
+        <Link to="/despre" onClick={() => isMobile && closeMobileMenu()}>
+          DESPRE
+        </Link>
+        <Link to="/produse" onClick={() => isMobile && closeMobileMenu()}>
+          PRODUSE
+        </Link>
+        <Link to="/proiecte" onClick={() => isMobile && closeMobileMenu()}>
+          PROIECTE
+        </Link>
+        <div onClick={() => setIsCartOpen(true)}>
+          <FiShoppingCart className="cart-icon" />
+        </div>
       </div>
     </div>
   );
